89ec8ad5f5b5c2771c4914b552590051dbe4c031
Tyshchenko Valeriia <128826458+TyshchenkoValeri@users.noreply.github.com>
2023-06-02 16:02:22 +0300

    error editing

--- tasks/lesson_13/examination.py
+++ tasks/lesson_13/examination.py
@@ -1,8 +1,7 @@
-import sys
+import argparse
 import requests
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
-from abc import ABC, abstractmethod
 
 
 class URLValidator:
@@ -12,25 +11,11 @@ class URLValidator:
         return bool(parsed.netloc) and bool(parsed.scheme)
 
 
-class LinkSaver(ABC):
-    @abstractmethod
-    def save_links_to_file(self, links, filename):
-        pass
-
-
-class FileLinkSaver(LinkSaver):
-    def save_links_to_file(self, links, filename):
-        with open(filename, "w") as file:
-            for link in links:
-                file.write(link + "\n")
-
-
 class HTMLParser:
-    def __init__(self, url, link_saver):
+    def __init__(self, url):
         self.url = url
         self.valid_links = []
         self.broken_links = []
-        self.link_saver = link_saver
 
     def get_valid_links(self):
         response = requests.get(self.url)
@@ -43,22 +28,31 @@ class HTMLParser:
             else:
                 self.broken_links.append(href)
 
+    def validate_links(self):
+        with open("status_200.txt", "w") as status_file, open("broken_links.txt", "w") as broken_file:
+            for link in self.valid_links:
+                response = requests.get(link)
+                if response.status_code == 200:
+                    status_file.write(link + "\n")
+                else:
+                    broken_file.write(link + "\n")
+
     def parse_html_page(self):
         if not URLValidator.is_valid_url(self.url):
             print("Invalid URL:", self.url)
             return
 
         self.get_valid_links()
-        self.link_saver.save_links_to_file(self.valid_links, "status_200.txt")
-        self.link_saver.save_links_to_file(self.broken_links, "broken_links.txt")
-
+        self.validate_links()
         print("Status 200 links saved to status_200.txt")
         print("Broken links saved to broken_links.txt")
 
 
 if __name__ == "__main__":
-    url = input("Enter the URL of the HTML page: ")
-    link_saver = FileLinkSaver()
-    html_parser = HTMLParser(url, link_saver)
-    html_parser.parse_html_page()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="URL of the HTML page")
+    args = parser.parse_args()
 
+    url = args.url
+    html_parser = HTMLParser(url)
+    html_parser.parse_html_page()
